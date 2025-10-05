7f6b247c42bff9c5b71274cd9fcb4f0902dcb149
Dias Ziyada <dias.ziyada12345@gmail.com>
2025-10-05 00:23:49 -0400

    Created new factories for the item and order models

--- tests/factories.py
+++ tests/factories.py
@@ -2,19 +2,45 @@
 Test Factory to make fake objects for testing
 """
 
+"""
+Test Factory to make fake objects for testing
+"""
+
 import factory
-from service.models import YourResourceModel
+import random
+from decimal import Decimal
+from service.models.orders import Order, OrderStatus
+from service.models.items import Item
 
 
-class YourResourceModelFactory(factory.Factory):
-    """Creates fake pets that you don't have to feed"""
+class OrderFactory(factory.Factory):
+    """Creates fake Order model instances for tests"""
 
     class Meta:  # pylint: disable=too-few-public-methods
-        """Maps factory to data model"""
+        model = Order
 
-        model = YourResourceModel
+    id = factory.Sequence(lambda n: n + 1)
+    customer_id = factory.Sequence(lambda n: n + 1)
+    status = OrderStatus.PENDING
+    total_price = factory.LazyFunction(
+        lambda: float(round(random.uniform(10.0, 200.0), 2))
+    )
+    # items should be a list (relationship), default empty list per-instance
+    items = factory.LazyFunction(list)
 
-    id = factory.Sequence(lambda n: n)
-    name = factory.Faker("first_name")
 
-    # Todo: Add your other attributes here...
+class ItemFactory(factory.Factory):
+    """Creates fake Item model instances for tests"""
+
+    class Meta:  # pylint: disable=too-few-public-methods
+        model = Item
+
+    id = factory.Sequence(lambda n: n + 1)
+    name = factory.Faker("word")
+    category = factory.Faker("word")
+    description = factory.Faker("sentence")
+    price = factory.LazyFunction(
+        lambda: Decimal(str(round(random.uniform(1.0, 100.0), 2)))
+    )
+    quantity = factory.LazyFunction(lambda: random.randint(1, 5))
+    order_id = factory.Sequence(lambda n: n + 1)
