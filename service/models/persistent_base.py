cc048f62fff9a875bec1b0825e0919984abe2357
Dias Ziyada <dias.ziyada12345@gmail.com>
2025-10-15 17:01:06 -0400

    fixed linting whitespace problem

--- service/models/persistent_base.py
+++ service/models/persistent_base.py
@@ -83,4 +83,3 @@ class PersistentBase:
         logger.info("Processing lookup for id %s ...", by_id)
         # pylint: disable=no-member
         return cls.query.session.get(cls, by_id)
-        
